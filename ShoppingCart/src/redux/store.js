5c6c8cc810ec7b242d85d2f3c1e7f82a03949236
srinibas <srinibaskhuntia39@gmail.com>
2021-12-30 20:38:14 +0530

    implemented add to cart and view items action

---
+++ ShoppingCart/src/redux/store.js
@@ -0,0 +1,9 @@
+import shopReducer from "./shopReducer";
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension" 
+
+
+const store=createStore(shopReducer,composeWithDevTools());
+
+
+export default store;
