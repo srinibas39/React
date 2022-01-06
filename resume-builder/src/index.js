5592fb816c0503a1068b601891120c16d16a6f95
srinibas <srinibaskhuntia39@gmail.com>
2022-01-07 01:25:10 +0530

    installed firebase

--- resume-builder/src/index.js
+++ resume-builder/src/index.js
@@ -3,19 +3,44 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; 
 import {Provider} from "react-redux"
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './redux/reducer/rootReducer';
-import {composeWithDevTools} from "redux-devtools-extension"
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import {reduxFirestore,getFirestore} from "redux-firestore";
+import { ReactReduxFirebaseProvider,getFirebase} from 'react-redux-firebase';
+import { createFirestoreInstance} from 'redux-firestore' // <- needed if using firestore
 
-const store=createStore(rootReducer,composeWithDevTools())
+
+const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),reduxFirestore(firebase)))//binding for redux to get firestore
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVXz0QVPRN4vPkXCCFNBb6syPISDlETzM",
+  authDomain: "resumeb-83234.firebaseapp.com",
+  projectId: "resumeb-83234",
+  storageBucket: "resumeb-83234.appspot.com",
+  messagingSenderId: "288267324655",
+  appId: "1:288267324655:web:819e200b080beefda55f1a"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
 
 ReactDOM.render(
     
-  <Provider store={store}>
     <BrowserRouter>
-      <App />
+        <Provider store={store}>
+          <ReactReduxFirebaseProvider
+            firebase={firebase}
+            config={firebaseConfig}
+            dispatch={store.dispatch}
+            createFirestoreInstance={createFirestoreInstance}>
+            <App />
+          </ReactReduxFirebaseProvider>
+        </Provider>  
     </BrowserRouter>
-  </Provider>  
 ,
   document.getElementById('root')
 );
