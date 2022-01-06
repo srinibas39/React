93676dc342de15e9a22f4471f72cee9bac665ded
srinibas <srinibaskhuntia39@gmail.com>
2022-01-06 15:57:16 +0530

    finalizePage.js

--- resume-builder/src/components/presentation/finalizePage.js
+++ resume-builder/src/components/presentation/finalizePage.js
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ResumePreview from './resumePreview'
 import  jsPDF  from "jspdf";
 import html2canvas from 'html2canvas';
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
    function Finalize(props) {
     let educationSection= props.educationSection
     let contactSection=props.contactSection
-    let documentd=props.document
+    let documentd=props.document;
+    const history=useHistory();
+
+    useEffect(()=>{
+      if(props.document.id==null){
+        history.push("/gettingStarted");
+      }
+    })
   
     const saveToDatabase= async()=>{
       //gets store in firebase
     }
      const downloadResume=()=> {
     
-       const input = document.getElementById('resumePreview');
-      console.log(document)
-       html2canvas(input)
-         .then((canvas) => {
-           const imgData = canvas.toDataURL('image/png');
-           const pdf = new jsPDF("p", "mm", "a4");
-           var width = pdf.internal.pageSize.getWidth();
-           var height = pdf.internal.pageSize.getHeight();
-           pdf.addImage(imgData, 'JPEG', 0, 0,width,height);
-           // pdf.output('dataurlnewwindow');
-           pdf.save("resume.pdf");
-         }).catch(function(error){
-           console.log(error)
-         })
+      //  const input = document.getElementById('resumePreview');
+      // console.log(document)
+      //  html2canvas(input)
+      //    .then((canvas) => {
+      //      const imgData = canvas.toDataURL('image/png');
+      //      const pdf = new jsPDF("p", "mm", "a4");
+      //      var width = pdf.internal.pageSize.getWidth();
+      //      var height = pdf.internal.pageSize.getHeight();
+      //      pdf.addImage(imgData, 'JPEG', 0, 0,width,height);
+      //      // pdf.output('dataurlnewwindow');
+      //      pdf.save("resume.pdf");
+      //    }).catch(function(error){
+      //      console.log(error)
+      //    })
+
+      const input=document.querySelector("#resumePreview");
+      html2canvas(input).then((canvas)=>{
+          const imgData=canvas.toDataURL("image/png");
+          const pdf=new jsPDF("p","mm","a4");
+          const width=pdf.internal.pageSize.getWidth();
+          const height=pdf.internal.pageSize.getHeight();
+          pdf.addImage(imgData,"JPEG",0,0,width,height);
+          pdf.save("resume.pdf");
+
+      }).catch((error)=>{
+          console.log(error);
+      })
      }
     return (
       <div className="container full finalize-page" >
@@ -54,9 +76,18 @@ import html2canvas from 'html2canvas';
     </div>
     )
 
+
     
 }
 
+const mapstateToProps=(state)=>{
+  return {
+    document:state.document,
+    educationSection:state.education,
+    contactSection:state.contactSection,
+
+  }
+}
 
 
-export default (Finalize)
+export default connect(mapstateToProps)(Finalize)
